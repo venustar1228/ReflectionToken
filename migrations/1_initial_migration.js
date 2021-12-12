7ff38b06815e326069f8e04d883048c464884e35
venustar1228 <venustar-1228@outlook.com>
2021-12-12 16:13:17 +0530

    Finish

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,35 @@
+const Token = artifacts.require("ReflectionToken");
+const Router = artifacts.require("IUniswapV2Router02");
+const currTime = Number(Math.round(new Date().getTime() / 1000));
+
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Token);
+
+  let tokenInstance = await Token.deployed();
+
+  await addLiq(tokenInstance, accounts[0]);
+
+};
+
+const addLiq = async (tokenInstance, account) => {
+
+  const routerInstance = await Router.at(
+    "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3"
+  );
+  
+  let supply = await tokenInstance.totalSupply();
+  await tokenInstance.approve(routerInstance.address, BigInt(supply), {
+    from: account,
+  });
+
+  await routerInstance.addLiquidityETH(
+    tokenInstance.address,
+    BigInt(supply / 2),
+    0,
+    0,
+    routerInstance.address,
+    currTime + 100,
+    { value: 1e16, from: account }
+  );
+
+}
